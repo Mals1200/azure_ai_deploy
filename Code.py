1f2a55f7ee0884e6bf207cc4ef263cc6268115d8
Mals1200 <168693176+Mals1200@users.noreply.github.com>
2024-10-28 16:28:36 +0300

    Update Code.py

--- Code.py
+++ Code.py
@@ -13,18 +13,14 @@ allowSelfSignedHttps(True) # this line is needed if you use self-signed certific
 # Request data goes here
 # The example below assumes JSON formatting which may be updated
 # depending on the format your endpoint expects.
-# More information can be found here:
-# https://docs.microsoft.com/azure/machine-learning/how-to-deploy-advanced-entry-script
 data = {"input":""}
 
 body = str.encode(json.dumps(data))
 
 url = 'https://cxqa-genai-project-fawqm.eastus.inference.ml.azure.com/score'
-# Replace this with the primary/secondary key, AMLToken, or Microsoft Entra ID token for the endpoint
-api_key = 'H8Tb0bn6NvwJU9BEgwu44zOmEL4eadgQ'
+api_key = 'H8Tb0bn6NvwJU9BEgwu44zOmEL4eadgQ'  # Make sure to use environment variables for security
 if not api_key:
     raise Exception("A key should be provided to invoke the endpoint")
-
 
 headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
 
@@ -32,12 +28,9 @@ req = urllib.request.Request(url, body, headers)
 
 try:
     response = urllib.request.urlopen(req)
-
     result = response.read()
     print(result)
 except urllib.error.HTTPError as error:
     print("The request failed with status code: " + str(error.code))
-
-    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
     print(error.info())
     print(error.read().decode("utf8", 'ignore'))
